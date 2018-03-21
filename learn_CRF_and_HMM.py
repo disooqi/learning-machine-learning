cf48aedf3f80ac7bdb0bb695c24d05e5b2ef9ea0
Mohamed Eldesouki <disooqi@gmail.com>
2018-03-21 14:03:27 +0300

    Create learn_CRF_and_HMM.py

---
+++ learn_CRF_and_HMM.py
@@ -0,0 +1,22 @@
+# http://www.chokkan.org/software/crfsuite/
+# http://www.albertauyeung.com/post/python-sequence-labelling-with-crf/
+# http://blog.echen.me/2012/01/03/introduction-to-conditional-random-fields/
+# http://homepages.inf.ed.ac.uk/csutton/publications/crftut-fnt.pdf
+# http://acepor.github.io/2017/03/06/CRF-Python/
+
+
+# Scikit learn justify the reason of not adding CRF to their core library as,
+# http://scikit-learn.org/stable/related_projects.html
+'''
+Not everything belongs or is mature enough for the central scikit-learn project. The following are projects providing interfaces 
+similar to scikit-learn for additional learning algorithms, infrastructures and tasks.
+
+Structured learning
+
+    Seqlearn Sequence classification using HMMs or structured perceptron.
+    HMMLearn Implementation of hidden markov models that was previously part of scikit-learn.
+    PyStruct General conditional random fields and structured prediction.
+    pomegranate Probabilistic modelling for Python, with an emphasis on hidden Markov models.
+    sklearn-crfsuite Linear-chain conditional random fields (CRFsuite wrapper with sklearn-like API).
+
+'''
