400e0ac8004c6ab14dee911cd8828f5ee25e2667
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2018-04-07 16:58:28 +0300

    Clean module nn from test lines

--- classic_NN/nn.py
+++ classic_NN/nn.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.io
 from scipy.special import expit, logit
 
 class HiddenLayer:
@@ -218,22 +217,4 @@ class NN:
 
 
 if __name__ == '__main__':
-    handwritten_digits = scipy.io.loadmat("../data/ex3data1.mat")
-    print(handwritten_digits.keys())
-    Xf = handwritten_digits['X'].T
-    yf = handwritten_digits['y'].T
-
-    print('There are {1:} training examples, each individual example is represented using {0:} features'.format(
-        *Xf.shape))
-    nn01 = NN(Xf, yf, loss='ff')
-    # nn01.add_layer(250, a activation='relu')
-    # nn01.add_layer(531, activation='relu')
-    # nn01.add_layer(250, activation='leaky_relu')
-    # nn01.add_layer(500, activation='tanh')
-    # nn01.add_layer(200, activation='tanh')
-    # nn01.add_layer(95, activation='tanh')
-    nn01.add_layer(25, activation='leaky_relu')
-
-    nn01.add_output_layer()
-    nn01.train(iterations=10000, alpha=0.1)
-    print(nn01.accuracy())
+    pass
